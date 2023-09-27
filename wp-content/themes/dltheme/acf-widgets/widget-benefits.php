75e9fedc8c262b52de1fafba1e488d858954cc77
dlaperishvili <davitlaperishvili@gmail.com>
2023-09-27 14:45:25 +0400

    new widgets in proress

---
+++ wp-content/themes/dltheme/acf-widgets/widget-benefits.php
@@ -0,0 +1,104 @@
+
+   <?php  
+        $benefits_style = get_sub_field('benefits_style'); 
+        
+        $rowIndex = get_row_index();
+    ?>
+    <?php 
+      if($benefits_style == "style_1"){
+        ?>
+          <section class="how-to-pick-real-money-slot-with-our-reviews">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <h2 class="how-to-pick-real-money-slot-with-our-reviews__title flex flex-justify-center flex-align-center">How to Pick Real Money Slot with Our Reviews</h2>
+                </div>
+                <div class="col-12">
+                  <div class="row how-to-pick-real-money-slot-with-our-reviews-list">
+                    <div class="col-12 col-md-6 col-lg-4">
+                      <div class="how-to-pick-real-money-slot-with-our-reviews-list__card">
+                        <picture class="render-image flex flex-align-center flex-justify-center picture-image- how-to-pick-real-money-slot-with-our-reviews-list__image">
+                          <source type="image/svg+xml" data-srcset="https://pokieslab.net/wp-content/themes/pokieslab/images/components/how-to-pick-real-money-slot-with-our-reviews/icon-croupier.svg" />
+                          <img
+                            alt="Return to Player"
+                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+                            width="100"
+                            height="120"
+                            class="lazy how-to-pick-real-money-slot-with-our-reviews-list__image"
+                            data-original="https://pokieslab.net/wp-content/themes/pokieslab/images/components/how-to-pick-real-money-slot-with-our-reviews/icon-croupier.svg"
+                          />
+                        </picture>
+                        <noscript>
+                          <img
+                            alt="Return to Player"
+                            src="https://pokieslab.net/wp-content/themes/pokieslab/images/components/how-to-pick-real-money-slot-with-our-reviews/icon-croupier.svg"
+                            width="100"
+                            height="120"
+                            class="lazy how-to-pick-real-money-slot-with-our-reviews-list__image"
+                          />
+                        </noscript>
+                        <span class="how-to-pick-real-money-slot-with-our-reviews-list__title">Return to Player </span>
+                        <p class="how-to-pick-real-money-slot-with-our-reviews-list__text"
+                          >RTP is supposed to cash or coins payable in winning cases with constant percent in a demo game and in payable with real money online pokies of Australia. It varies 86—99.9% in
+                          popular slot machine emulators. Test RTP in no money mode and continue for cash.</p
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        <?php
+      }else{
+        ?>
+          <section class="real-money-bonuses-real-money-page">
+            <div class="container real-money-bonuses-real-money-page__container">
+              <div class="row flex-justify-start">
+                <div class="col-12 col-lg-6 flex flex-justify-start">
+                  <h2 class="real-money-bonuses-real-money-page__title">Real Money Bonuses</h2>
+                </div>
+                <div class="col-12 col-md-6 col-lg-12 real-money-bonuses-real-money-page__card-wrapper">
+                  <div id="real-money-bonuses-real-money-page-card-1" class="real-money-bonuses-real-money-page-card">
+                    <div class="row">
+                      <div class="col-12 col-lg-3 flex flex-justify-center flex-align-center">
+                        <picture class="render-image flex flex-align-center flex-justify-center picture-image- real-money-bonuses-real-money-page-card__image">
+                          <source type="image/svg+xml" data-srcset="https://pokieslab.net/wp-content/themes/pokieslab/images/components/real-money-bonuses/042-casino-chips.svg" />
+                          <img
+                            alt="No Deposit Bonus"
+                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+                            width="120"
+                            height="120"
+                            class="lazy real-money-bonuses-real-money-page-card__image"
+                            data-original="https://pokieslab.net/wp-content/themes/pokieslab/images/components/real-money-bonuses/042-casino-chips.svg"
+                          />
+                        </picture>
+                        <noscript>
+                          <img
+                            alt="No Deposit Bonus"
+                            src="https://pokieslab.net/wp-content/themes/pokieslab/images/components/real-money-bonuses/042-casino-chips.svg"
+                            width="120"
+                            height="120"
+                            class="lazy real-money-bonuses-real-money-page-card__image"
+                          />
+                        </noscript>
+                      </div>
+                      <div class="col-12 col-lg-9">
+                        <div class="real-money-bonuses-real-money-page-card__content">
+                          <h4 class="real-money-bonuses-real-money-page-card__title">No Deposit Bonus</h4>
+                          <p class="real-money-bonuses-real-money-page-card__text"
+                            >A no deposit bonus provides funds to play with no depositing free cash. The largest is AU$10,000 Bonus + 400 Free Spins on PokiesLab online casinos given instantly after
+                            depositing a minimum of AU$20. Meet the playthrough requirement to cash out no deposit bonus wins.</p
+                          >
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        <?php
+      }
+    ?>
+
